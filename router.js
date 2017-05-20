79a86750b296ce03dbc714c06fe27f64f2a2128e
David Spreekmeester <david@grrr.nl>
2017-05-20 23:26:40 +0200

    Restructured router into namespaces

--- router.js
+++ router.js
@@ -1,49 +1,13 @@
 #!/usr/bin/env node
 //var util = require('util')
 var sc = require('subcommander');
-var db = require('./lib/db.js');
+var db = require('./lib/db.js').commands(sc);
  
 sc.command( 'version', {
     desc: 'display app\'s version',
     callback: function () {
         // @todo: show package.json version
         console.log( 'version' );
-    }
-} );
- 
-var dbc = sc.command('db', {
-        desc: 'Handle database content'
-    })
-    /*
-    .option( 'port', {
-        abbr: 'p',
-        desc: 'Server port',
-        default: '8080'
-    } )
-    .option( 'hostname', {
-        abbr: 'H',
-        desc: 'Server hostname'
-    } );
-   */
-;
-    
-dbc.command('migrate', {
-    desc: 'Migrate database content between environments',
-    callback: function ( options ) {
-        // @todo
-        db.migrate();
-        //var port = options.port,
-        //hostname = options.hostname;
- 
-        //console.log( port, hostname );
-    }
-} );
- 
-dbc.command('backup', {
-    desc: 'Backup a database',
-    callback: function () {
-        db.backup();
-        // @todo
     }
 } );
  
