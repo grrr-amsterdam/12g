b39aad7f04e478bc79d80624dbf815c052d48070
David Spreekmeester <david@grrr.nl>
2017-05-24 04:55:24 +0200

    Added feature: env template
    * It's now possible to create templates of the local .env file
    * Updated deps
    * Added env routing

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # 12g
 Administration tool for development and devops, according to the 12factor principles as applied by [Grrr](https://github.com/grrr-amsterdam)
 
-Work in progress, not functioning as of yet.
+
+## Install
+```bash
+$ npm install -g 12g
+```
+
+
+## Commands
+
+### Display help
+```bash
+$ 12g -h [command] [subcommand]
+```
+
+### Environments
+#### Create a template .env file
+```bash
+$ 12g env template
+```
+This will create a copy of the `.env` file, named `.env.template`, with all values emptied.
--- router.js
+++ router.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 //var util = require('util')
 var sc = require('subcommander');
-var db = require('./lib/db/router.js').commands(sc);
+require('./lib/db/router.js').commands(sc);
+require('./lib/env/router.js').commands(sc);
  
+sc.option( 'help', {
+    abbr: 'h',
+    desc: 'Display help'
+})
+
 sc.command( 'version', {
     desc: 'display app\'s version',
     callback: function () {
         // @todo: show package.json version
         console.log( 'version' );
     }
-} );
- 
+})
+  
 sc.parse();
