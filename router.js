5df90039f66d0df4bddfaf2734459c3a3cae0526
David Spreekmeester <david@grrr.nl>
2017-05-25 06:06:09 +0200

    Added version functionality and moved into it's own router

--- router.js
+++ router.js
@@ -1,20 +1,16 @@
 #!/usr/bin/env node
-//var util = require('util')
+/**
+ * @author David Spreekmeester <david@grrr.nl>
+ */
+
 var sc = require('subcommander');
 require('./lib/db/router.js').commands(sc);
 require('./lib/env/router.js').commands(sc);
+require('./lib/version/router.js').commands(sc);
  
 sc.option( 'help', {
     abbr: 'h',
     desc: 'Display help'
 })
 
-sc.command( 'version', {
-    desc: 'display app\'s version',
-    callback: function () {
-        // @todo: show package.json version
-        console.log( 'version' );
-    }
-})
-  
 sc.parse();
