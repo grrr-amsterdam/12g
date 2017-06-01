2a07ee422d151e78ad6f6b147080ed5bd765ba08
David Spreekmeester <david@grrr.nl>
2017-06-01 17:06:48 +0200

    Refactoring + added -v root option for version

--- router.js
+++ router.js
@@ -3,6 +3,41 @@
  * @author David Spreekmeester <david@grrr.nl>
  */
 
+const _ = require('lodash')
+const version = require('./lib/version/version.js')
+
+
+/**
+ * This method returns the cli arguments, to override Subcommander
+ * @return string The cli arguments passed to this app
+ */
+function getArguments() {
+    var args = []
+    var baseCommandPos = false
+
+    for (var i in process.argv) {
+        if (
+            baseCommandPos !== false &&
+            i > (baseCommandPos)
+        ) {
+            args.push(process.argv[i])
+        }
+
+        if (
+            (
+                _.endsWith(process.argv[i], '.js') ||
+                _.endsWith(process.argv[i], '12g')
+            ) && baseCommandPos === false
+        ) {
+            // This is the executable or the entry node script
+            baseCommandPos = Number(i)
+        }
+    }
+
+    return args.join(' ')
+}
+
+var args = getArguments()
 var sc = require('subcommander');
 require('./lib/db/router.js').commands(sc);
 require('./lib/env/router.js').commands(sc);
@@ -13,5 +48,14 @@ sc.option( 'help', {
     abbr: 'h',
     desc: 'Display help'
 })
+.option('version', {
+    abbr: 'v',
+    desc: 'Display version'
+})
 
-sc.parse();
+var versionArgs = ['-v', '--v', '--version']
+if (versionArgs.includes(args)) {
+    console.log(version.get())
+} else {
+    sc.parse()
+}
